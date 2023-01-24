27849145950302f28fa89b23be3200d1254c1be3
David Dvali <david.dvali@gmail.com>
2023-01-24 21:03:56 +0400

    Starting DAL implementation. Include login method

--- G07_AddressBook/AddressBook.DAL/RepositoryBase.cs
+++ G07_AddressBook/AddressBook.DAL/RepositoryBase.cs
@@ -1,6 +1,14 @@
-﻿namespace AddressBook.DAL
+﻿using DatabaseHelper;
+
+namespace AddressBook.DAL
 {
 	public abstract class RepositoryBase
 	{
+		protected MsSqlDatabase _database;
+
+		protected RepositoryBase()
+		{
+			_database = new MsSqlDatabase("");
+		}
 	}
 }
--- G07_AddressBook/AddressBook.DAL/UserRepository.cs
+++ G07_AddressBook/AddressBook.DAL/UserRepository.cs
@@ -1,24 +1,30 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlClient;
 using AddressBook.DTO;
 
 namespace AddressBook.DAL
 {
 	//todo: დავამატოთ ყველა საჭირო ფუნქცია ბაზის პროცედურების მიხედვით.
-    public sealed class UserRepository : RepositoryBase
-    {
-	    public void Register(User user)
-	    {
-		    throw new NotImplementedException();
-	    }
+	public sealed class UserRepository : RepositoryBase
+	{
+		public void Register(User user)
+		{
+			throw new NotImplementedException();
+		}
 
-	    public string Login(string email, string password)
-	    {
-		    throw new NotImplementedException();
-	    }
+		public string Login(string email, string password)
+		{
+			return _database.ExecuteScalar(
+				"UserLogin_SP",
+				CommandType.StoredProcedure,
+				new SqlParameter("Email", email),
+				new SqlParameter("Password", password)).ToString();
+		}
 
-	    public void SetPassword(string currentPassword, string password)
-	    {
-		    throw new NotImplementedException();
-	    }
-    }
+		public void SetPassword(string currentPassword, string password)
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
