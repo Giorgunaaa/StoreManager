92bbbf493b357b52aaf549825b2c8439bff3e373
lasha1008 <lasha1008@gmail.com>
2023-02-23 23:09:21 +0400

    MyUnion added

--- Group-05/Extensions/MyEnumerable.cs
+++ Group-05/Extensions/MyEnumerable.cs
@@ -15,4 +15,24 @@ public static class MyEnumerable
 			}
 		}
 	}
+
+    public static IEnumerable<T> MyUnion<T>(this IEnumerable<T> firstEnumerable, IEnumerable<T> secondEnumerable)
+    {
+        if (firstEnumerable == null) throw new ArgumentNullException(nameof(firstEnumerable));
+        if (secondEnumerable == null) throw new ArgumentNullException(nameof(secondEnumerable));
+
+        HashSet<T> result = new HashSet<T>();
+
+        foreach (var item in firstEnumerable)
+        {
+            result.Add(item);
+        }
+
+        foreach (var item in secondEnumerable)
+        {
+            result.Add(item);
+        }
+
+        return result;
+    }
 }
