8d70a1c8f3576fe9e768611a4e49c11d5afeb8c1
David Dvali <59212224+daviddvali@users.noreply.github.com>
2023-05-19 20:46:37 +0400

    Change Dto

--- G07_StoreManager/StoreManager.DTO/Product.cs
+++ G07_StoreManager/StoreManager.DTO/Product.cs
@@ -1,27 +1,25 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace StoreManager.DTO
+namespace StoreManager.DTO;
+
+public class Product
 {
-    public class Product
-    {
-        [Key]
-        public int Id { get; set; }
+    [Key]
+    public int Id { get; set; }
 
-        [Required]
-        [MaxLength(50)]
-        public string Name { get; set; } = null!;
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; } = null!;
 
-        public string? Description { get; set; }
+    [MaxLength(1000)]
+    public string? Description { get; set; }
 
-        [Column(TypeName = "money")]
-        public decimal Price { get; set; }
+    [Required]
+    [Column(TypeName = "money")]
+    public decimal Price { get; set; }
+    
+    public bool IsDeleted { get; set; }
 
-        [Column(TypeName = "smallint")]
-        public short Quantity { get; set; }
-
-        public bool IsDeleted { get; set; }
-
-        public ICollection<Category>? Categories { get; set; }
-    }
+    public ICollection<Category>? Categories { get; set; }
 }
