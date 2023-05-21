4f6e499bde2b41ffe467da222c08ce81953ea280
lasha1008 <lasha1008@gmail.com>
2023-05-21 23:38:03 +0400

    3 DTO classes added customer, employee and person(abstract).

---
+++ G07_StoreManager/StoreManager.DTO/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StoreManager.DTO
+{
+    public sealed class Customer : Person
+    {
+        [MaxLength(20)]
+        public string UserName { get; set; } = null!;
+
+        [Column(TypeName = "money")]
+        public decimal AccountBalance { get; set; }
+    }
+}
---
+++ G07_StoreManager/StoreManager.DTO/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StoreManager.DTO
+{
+    public sealed class Employee : Person
+    {
+        [MaxLength(30)]
+        public string? Prosition { get; set; }
+
+        public Employee? ReportsTo { get; set; }
+
+        public ICollection<Employee>? ReportsFrom { get; set; }
+    }
+}
