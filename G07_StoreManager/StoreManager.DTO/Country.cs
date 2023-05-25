bac7da0e3d29e3fd899f44411152ee89e2967933
lasha1008 <lasha1008@gmail.com>
2023-05-25 22:26:44 +0400

    City and Country DTO added. City related to Employee (many to many).

---
+++ G07_StoreManager/StoreManager.DTO/Country.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StoreManager.DTO
+{
+    public class Country
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Name { get; set; } = null!;
+
+        public ICollection<City>? Cities { get; set; }
+    }
+}
--- G07_StoreManager/StoreManager.DTO/Employee.cs
+++ G07_StoreManager/StoreManager.DTO/Employee.cs
@@ -2,12 +2,14 @@
 
 namespace StoreManager.DTO;
 
-    public sealed class Employee : Person
-    {
-        [MaxLength(30)]
-        public string? Prosition { get; set; }
+public sealed class Employee : Person
+{
+    [MaxLength(30)]
+    public string? Prosition { get; set; }
 
-        public Employee? ReportsTo { get; set; }
+    public Employee? ReportsTo { get; set; }
 
-        public ICollection<Employee>? ReportsFrom { get; set; }
-    }
+    public ICollection<Employee>? ReportsFrom { get; set; }
+
+    public ICollection<City>? Cities { get; set; }
+}
