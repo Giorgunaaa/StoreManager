6908c54c8e48fa36be697ae098b66b6c3dbb6cb0
lasha1008 <lasha1008@gmail.com>
2023-06-04 02:18:29 +0400

    GetHash string extention completed.

--- G07_StoreManager/StoreManager.Facade/HelpExtentions/StringExtentions.cs
+++ G07_StoreManager/StoreManager.Facade/HelpExtentions/StringExtentions.cs
@@ -1,10 +1,24 @@
-﻿namespace StoreManager.Facade.HelpExtentions;
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace StoreManager.Facade.HelpExtentions;
 
 public static class StringExtentions
 {
-    public static string GetHash(this string text)
+    public static string GetHash(this string input)
     {
+        using (SHA256 sha256 = SHA256.Create())
+        {
+            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+            byte[] hashBytes = sha256.ComputeHash(inputBytes);
 
-        return text;
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < hashBytes.Length; i++)
+            {
+                builder.Append(hashBytes[i].ToString("x2"));
+            }
+
+            return builder.ToString();
+        }
     }
 }
