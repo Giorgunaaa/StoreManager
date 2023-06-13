798118f5f0683929bea8b9bb8f2fd5bf0d369df7
lasha1008 <lasha1008@gmail.com>
2023-06-14 01:41:22 +0400

    DI added to ICategoryRepository in unit tests.

---
+++ G07_StoreManager/StoreManager.Tests/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using StoreManager.Facade.Interfaces;
+using StoreManager.Repositories;
+
+namespace StoreManager.Tests;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        var options = new DbContextOptionsBuilder<StoreManagerDbContext>()
+       .UseSqlServer("server = LAPTOP-KJO35V1P; database = G07_StoreManager; integrated security = true; TrustServerCertificate = true")
+       .Options;
+
+        services.AddScoped(_ => new StoreManagerDbContext(options));
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
+    }
+}
