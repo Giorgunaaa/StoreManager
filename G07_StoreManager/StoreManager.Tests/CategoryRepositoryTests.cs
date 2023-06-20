f5232444cf1ea5b4239027820fdecacd538046eb
lasha1008 <lasha1008@gmail.com>
2023-06-20 18:10:22 +0400

    CategoryRepositoryTests functions: Insert, Update, Delete changed from Fact to Theory and simple inline data added.

--- G07_StoreManager/StoreManager.Tests/CategoryRepositoryTests.cs
+++ G07_StoreManager/StoreManager.Tests/CategoryRepositoryTests.cs
@@ -5,61 +5,73 @@ namespace StoreManager.Tests;
 
 public class CategoryRepositoryTests : RepositoryUnitTestBase
 {
-	private readonly ICategoryRepository _categoryRepository;
+    private readonly ICategoryRepository _categoryRepository;
 
-	public CategoryRepositoryTests(ICategoryRepository categoryRepository)
-	{
-		_categoryRepository = categoryRepository ?? throw new ArgumentNullException(nameof(categoryRepository));
-	}
+    public CategoryRepositoryTests(ICategoryRepository categoryRepository)
+    {
+        _categoryRepository = categoryRepository ?? throw new ArgumentNullException(nameof(categoryRepository));
+    }
 
-	[Fact]
-	public void Insert()
-	{
-		var category = GetTestRecord();
-		_categoryRepository.Insert(category);
-		_categoryRepository.SaveChanges();
+    [Theory]
+    [InlineData("Category 1", "Description 1")]
+    [InlineData("Category 2", "Description 2")]
+    [InlineData("Category 3", "Description 3")]
+    [InlineData("Category 4", "Description 4")]
+    public void Insert(string name, string description)
+    {
+        var category = GetTestRecord(name, description);
+        _categoryRepository.Insert(category);
+        _categoryRepository.SaveChanges();
 
-		Assert.True(category.Id > 0);
-	}
+        Assert.True(category.Id > 0);
+    }
 
-	[Fact]
-	public void Update()
-	{
-		var newCategory = GetTestRecord();
-		_categoryRepository.Insert(newCategory);
-		_categoryRepository.SaveChanges();
+    [Theory]
+    [InlineData("Category 1", "Description 1")]
+    [InlineData("Category 2", "Description 2")]
+    [InlineData("Category 3", "Description 3")]
+    [InlineData("Category 4", "Description 4")]
+    public void Update(string name, string description)
+    {
+        var newCategory = GetTestRecord(name, description);
+        _categoryRepository.Insert(newCategory);
+        _categoryRepository.SaveChanges();
 
-		newCategory.Name = "New Category 1";
-		newCategory.Description = "New Test Description.";
-		_categoryRepository.Update(newCategory);
-		_categoryRepository.SaveChanges();
+        newCategory.Name = "New Category 1";
+        newCategory.Description = "New Test Description.";
+        _categoryRepository.Update(newCategory);
+        _categoryRepository.SaveChanges();
 
-		var updatedCategory = _categoryRepository.Set(x => x.Id == newCategory.Id).Single();
+        var updatedCategory = _categoryRepository.Set(x => x.Id == newCategory.Id).Single();
 
-		Assert.True(updatedCategory.Name == "New Category 1" && updatedCategory.Description == "New Test Description.");
-	}
+        Assert.True(updatedCategory.Name == "New Category 1" && updatedCategory.Description == "New Test Description.");
+    }
 
-	[Fact]
-	public void Delete()
-	{
-		var newCategory = GetTestRecord();
-		_categoryRepository.Insert(newCategory);
-		_categoryRepository.SaveChanges();
-		
-		var existingCategory = _categoryRepository.Set(x => x.Id == newCategory.Id).Single();
-		_categoryRepository.Delete(existingCategory);
-		_categoryRepository.SaveChanges();
-		
-		Assert.Null(_categoryRepository.Set(x => x.Id == newCategory.Id).SingleOrDefault());
-	}
+    [Theory]
+    [InlineData("Category 1", "Description 1")]
+    [InlineData("Category 2", "Description 2")]
+    [InlineData("Category 3", "Description 3")]
+    [InlineData("Category 4", "Description 4")]
+    public void Delete(string name, string description)
+    {
+        var newCategory = GetTestRecord(name, description);
+        _categoryRepository.Insert(newCategory);
+        _categoryRepository.SaveChanges();
 
-	private static Category GetTestRecord()
-	{
-		Category category = new()
-		{
-			Name = "Category 1",
-			Description = "Test Description."
-		};
-		return category;
-	}
+        var existingCategory = _categoryRepository.Set(x => x.Id == newCategory.Id).Single();
+        _categoryRepository.Delete(existingCategory);
+        _categoryRepository.SaveChanges();
+
+        Assert.Null(_categoryRepository.Set(x => x.Id == newCategory.Id).SingleOrDefault());
+    }
+
+    private static Category GetTestRecord(string name, string description)
+    {
+        Category category = new()
+        {
+            Name = name,
+            Description = description
+        };
+        return category;
+    }
 }
