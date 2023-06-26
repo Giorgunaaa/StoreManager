3df82c4412c876deda6ee738fc0d473bffcd6605
lasha1008 <lasha1008@gmail.com>
2023-06-27 01:07:47 +0400

    CategoryRepositoryUnitTest changes.

--- G07_StoreManager/StoreManager.Tests/Startup.cs
+++ G07_StoreManager/StoreManager.Tests/Startup.cs
@@ -10,14 +10,6 @@ public class Startup
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddDbContext<StoreManagerDbContext>(options => options.UseInMemoryDatabase(databaseName: "MyTestDatabase"));
-        services.AddScoped<ICategoryRepository, CategoryRepository>();
-        services.AddScoped<ICityRepository, CityRepository>();
-        services.AddScoped<ICountryRepository, CountryRepository>();
-        services.AddScoped<ICustomerRepository, CustomerRepository>();
-        services.AddScoped<IEmployeeRepository, EmployeeRepository>();
-        services.AddScoped<IOrderDetailsRepository, OrderDetailsRepository>();
-        services.AddScoped<IOrderRepository, OrderRepository>();
-        services.AddScoped<IProductRepository, ProductRepository>();
-        services.AddSingleton<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
     }
 }
