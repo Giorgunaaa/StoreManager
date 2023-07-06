1778cacbe6f0ad168f27c238efbfeac2915d1b5c
lasha1008 <lasha1008@gmail.com>
2023-07-07 02:11:30 +0400

    AccountDetails DTO class added for Employee and configurated unique username.

---
+++ G07_StoreManager/StoreManager.DTO/AccountDetails.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StoreManager.DTO;
+
+public class AccountDetails
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(20)]
+    public string Username { get; set; } = null!;
+
+    [Required]
+    [MaxLength(20)]
+    public string Password { get; set; } = null!;
+}
--- G07_StoreManager/StoreManager.DTO/Employee.cs
+++ G07_StoreManager/StoreManager.DTO/Employee.cs
@@ -7,13 +7,9 @@ public sealed class Employee : Person
     [MaxLength(30)]
     public string? Position { get; set; }
 
-    [MaxLength(20)]
-    public string Username { get; set; } = null!;
-
-    [MaxLength(20)]
-    public string Password { get; set; } = null!;
-
     public Employee? ReportsTo { get; set; }
+
+    public AccountDetails? AccountDetails { get; set; }
 
     public ICollection<Employee>? ReportsFrom { get; set; }
 
