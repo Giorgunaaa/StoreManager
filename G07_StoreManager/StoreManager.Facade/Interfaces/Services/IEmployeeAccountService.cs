93a8bcd3da7a46bd8c95697203601a3b0852736f
lasha1008 <lasha1008@gmail.com>
2023-07-07 02:12:07 +0400

    Some methods added to EmployeeAccountService class.

--- G07_StoreManager/StoreManager.Facade/Interfaces/Services/IEmployeeAccountService.cs
+++ G07_StoreManager/StoreManager.Facade/Interfaces/Services/IEmployeeAccountService.cs
@@ -1,9 +1,8 @@
 ﻿using StoreManager.DTO;
-using StoreManager.Facade.Interfaces.Services;
 
 namespace StoreManager.Facade.Interfaces.Services;
 
 public interface IEmployeeAccountService : IQueryService<Employee>
 {
-    
+
 }
