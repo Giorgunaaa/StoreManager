200a95e7ad59078a663dfa61051197431ff6a356
lasha1008 <lasha1008@gmail.com>
2023-07-10 00:57:50 +0400

    simple unit test for EmployeeAcountService Register.

---
+++ G07_StoreManager/StoreManager.Tests/EmployeeAccountServiceTests.cs
@@ -0,0 +1,41 @@
+﻿using StoreManager.DTO;
+using StoreManager.Facade.Interfaces.Repositories;
+using StoreManager.Services;
+
+namespace StoreManager.Tests;
+
+public class EmployeeAccountServiceTests
+{
+    private readonly EmployeeAccountService _employeeAccountService;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public EmployeeAccountServiceTests(IUnitOfWork unitOfWork)
+    {
+        _employeeAccountService = new EmployeeAccountService(unitOfWork);
+        _unitOfWork = unitOfWork;
+    }
+
+    [Fact]
+    public void Register()
+    {
+        Employee employee = new Employee()
+        {
+            FirstName = "name111",
+            LastName = "lastname111",
+            AccountDetails = new()
+            {
+                Password = "pass",
+                Username = "user111"
+            }
+        };
+
+        _employeeAccountService.Register(employee);
+
+        Employee? expectedEmployee = _unitOfWork.EmployeeRepository
+            .Set()
+            .SingleOrDefault(x => x.Id == employee.Id);
+
+        Assert.NotNull(expectedEmployee);
+
+    }
+}
