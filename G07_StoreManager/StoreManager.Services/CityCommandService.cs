82bb5ffae7f74292a2480c5eef54d186750b3344
Shunsui <Shunsui@DESKTOP-TVFTMCI>
2023-07-21 14:30:10 +0400

    CityServices add

--- G07_StoreManager/StoreManager.Services/CityCommandService.cs
+++ G07_StoreManager/StoreManager.Services/CityCommandService.cs
@@ -1,12 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using StoreManager.DTO;
+using StoreManager.Facade.Interfaces.Repositories;
+using StoreManager.Facade.Interfaces.Services;
 
-namespace StoreManager.Services
-{
-    internal class CityCommandService
+namespace StoreManager.Services;
+
+public sealed class CityCommandService : ICityCommandService
+{ 
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CityCommandService(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+
+    public int Insert(City entity)
     {
+        if (entity == null) throw new ArgumentNullException(nameof(entity));
+
+        _unitOfWork.CityRepository.Insert(entity);
+        _unitOfWork.SaveChanges();
+
+        return entity.Id;
+    }
+
+    public void Update(City entity)
+    {
+        if (entity == null) throw new ArgumentNullException(nameof(entity));
+
+        _unitOfWork.CityRepository.Update(entity);
+        _unitOfWork.SaveChanges();
+    }
+
+    public void Delete(City entity)
+    {
+        if (entity == null) throw new ArgumentNullException(nameof(entity));
+
+        entity.IsDeleted = true;
+        _unitOfWork.CityRepository.Update(entity);
+        _unitOfWork.SaveChanges();
     }
 }
--- G07_StoreManager/StoreManager.Services/CityQueryService.cs
+++ G07_StoreManager/StoreManager.Services/CityQueryService.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using StoreManager.DTO;
+using StoreManager.Facade.Interfaces.Repositories;
+using StoreManager.Facade.Interfaces.Services;
+using System.Linq.Expressions;
 
-namespace StoreManager.Services
+namespace StoreManager.Services;
+
+public sealed class CityQueryService : ICityQueryService
 {
-    internal class CityQueryService
-    {
-    }
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CityQueryService(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+
+    City IQueryService<City>.Get(params object[] id) => _unitOfWork
+        .CityRepository
+        .Set()
+        .Single(x => x.Id == Convert.ToInt32(id) && !x.IsDeleted);
+
+    public IEnumerable<City> Set(Expression<Func<City, bool>> predicate) => _unitOfWork
+        .CityRepository
+        .Set(predicate);
+
+    IEnumerable<City> IQueryService<City>.Set() => _unitOfWork.CityRepository.Set();
 }
