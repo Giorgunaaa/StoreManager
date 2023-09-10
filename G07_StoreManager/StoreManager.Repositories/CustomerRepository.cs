077122ab470eb0ecdc444d502361821bbc24d70c
lasha1008 <lasha1008@gmail.com>
2023-09-10 20:34:34 +0400

    DeleteAccount method implemented in CustomerRepository.

--- G07_StoreManager/StoreManager.Repositories/CustomerRepository.cs
+++ G07_StoreManager/StoreManager.Repositories/CustomerRepository.cs
@@ -6,4 +6,11 @@ namespace StoreManager.Repositories;
 internal sealed class CustomerRepository : RepositoryBase<Customer>, ICustomerRepository
 {
     public CustomerRepository(StoreManagerDbContext context) : base(context) { }
+
+    public void DeleteAccount(Customer customer)
+    {
+        if (customer == null) throw new ArgumentNullException(nameof(customer));
+        AccountDetails? accountDetails = _context.AccountDetails.Find(customer.Id);
+        _context.AccountDetails.Remove(accountDetails!);
+    }
 }
