2fdb9deee90226536751c897694a54a809783ab8
lasha1008 <lasha1008@gmail.com>
2023-10-23 21:23:32 +0400

    Exception handler moved to extension.

--- G07_StoreManager/StoreManager.API/Program.cs
+++ G07_StoreManager/StoreManager.API/Program.cs
@@ -1,18 +1,19 @@
-using System.Net;
-using System.Text.Json;
-using Microsoft.AspNetCore.Diagnostics;
 using Serilog;
 using StoreManager.API.Configuration;
+using StoreManager.API.ExceptionHandler;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 
 var logger = new LoggerConfiguration()
-    .ReadFrom.Configuration(builder.Configuration)
-    .Enrich.FromLogContext()
+    .WriteTo.Console(restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information)
+    .WriteTo.File("logs/log-.txt",
+        restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information,
+        rollingInterval: RollingInterval.Day)
     .CreateLogger();
 
 builder.Logging.AddSerilog(logger);
+builder.Host.UseSerilog();
 
 builder.Services.AddControllers();
 
@@ -28,32 +29,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 
-app.UseExceptionHandler(appError =>
-{
-	appError.Run(async context =>
-	{
-		context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
-		context.Response.ContentType = "application/json";
-
-		var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
-		if (contextFeature != null)
-		{
-			logger.Error(contextFeature.Error, "Application Error");
-			await context.Response.WriteAsync(
-				new ErrorDetails(context.Response.StatusCode, "Internal Server Error."
-				).ToString());
-		}
-	});
-});
+app.HandleException();
 
 app.Run();
-
-internal sealed record ErrorDetails(int StatusCode, string Message)
-{
-	public override string ToString() => JsonSerializer.Serialize(this);
-}
