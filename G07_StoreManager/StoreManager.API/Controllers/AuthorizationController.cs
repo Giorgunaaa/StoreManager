bae10bea989043c5e7279e9ac518216128767710
lasha1008 <lasha1008@gmail.com>
2023-11-03 16:08:51 +0400

    Authorization added

---
+++ G07_StoreManager/StoreManager.API/Controllers/AuthorizationController.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using StoreManager.Facade.Interfaces.Services;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace StoreManager.API.Controllers;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+public class AuthorizationController : ControllerBase
+{
+    private readonly IAccountService _accountService;
+    private readonly IMapper _mapper;
+    private readonly IConfiguration _configuration;
+
+    public AuthorizationController(IAccountService accountService, IMapper mapper, IConfiguration configuration)
+    {
+        _accountService = accountService;
+        _mapper = mapper;
+        _configuration = configuration;
+    }
+
+    [HttpPost]
+    public IActionResult Login(string username, string password)
+    {
+        if (username == "Admin" && password == "admin")
+        {
+            var issuer = _configuration["Jwt:Issuer"];
+            var audience = _configuration["Jwt:Audience"];
+            var key = Encoding.ASCII.GetBytes(_configuration["Jwt:Key"]);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                new Claim("Id", Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Sub, username),
+                new Claim(JwtRegisteredClaimNames.Email, username),
+                new Claim(JwtRegisteredClaimNames.Jti,
+                Guid.NewGuid().ToString())
+             }),
+                Expires = DateTime.UtcNow.AddMinutes(5),
+                Issuer = issuer,
+                Audience = audience,
+                SigningCredentials = new SigningCredentials
+                (new SymmetricSecurityKey(key),
+                SecurityAlgorithms.HmacSha512Signature)
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var jwtToken = tokenHandler.WriteToken(token);
+            var stringToken = tokenHandler.WriteToken(token);
+            return Ok(stringToken);
+        }
+        return Unauthorized();
+    }
+}
